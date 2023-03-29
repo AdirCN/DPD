f4edefcdf7af3a0ffc61de7fb1656ddc6152c5c6
AdirCN1234 <adir20019@gmail.com>
2023-03-29 13:33:17 +0300

    29.3.23

--- code/neural.py
+++ code/neural.py
@@ -6,32 +6,50 @@ import torch
 import torch.nn as nn
 
 with open('nn_input.npy', 'rb') as f:
-	nn_input = torch.from_numpy(np.load(f))
+	nn_input = torch.from_numpy(np.load(f)/10000000000)
 with open('nn_output.npy', 'rb') as f:
-	nn_output = torch.from_numpy(np.load(f))
+	real_output = np.load(f)
+	nn_output = torch.from_numpy(real_output/10000000000)
 
 m = 2
 k = 9
 inputLayerDim = (2+k)*m
-n_input, n_hidden, n_out, learning_rate = inputLayerDim, 4, 2, 0.001
+n_input, n_hidden, n_out, learning_rate = inputLayerDim, 40, 2, 0.001
 model = nn.Sequential(nn.Linear(n_input, n_hidden),
-					  nn.ReLU(),
-					  nn.Linear(n_hidden, n_out))
+					nn.ReLU(),
+					nn.Linear(n_hidden, n_hidden),
+					nn.ReLU(),
+					nn.Linear(n_hidden, n_hidden),
+					nn.ReLU(),
+					nn.Linear(n_hidden, n_out)
+					)
 loss_function = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 losses = []
-for epoch in range(5000):
+for epoch in range(500):
 	pred_y = model(nn_input.float())
 	loss = loss_function(pred_y, nn_output.float())
 	losses.append(loss.item())
 	model.zero_grad()
 	loss.backward()
 	optimizer.step()
-	
+
 import matplotlib.pyplot as plt
 plt.plot(losses)
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.title("Learning rate %f"%(learning_rate))
 plt.show()
+
+t = np.linspace(0,20,10500)
+s = nn_output.float().detach().numpy()
+p = pred_y.detach().numpy()
+
+
+plt.plot(t, s)
+plt.show()
+
+plt.plot(t, p)
+plt.show()
+
