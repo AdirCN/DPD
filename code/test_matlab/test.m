44b716335282dac16dfa20ed1fc380ca0489dbea
AdirCN1234 <adir20019@gmail.com>
2023-04-24 21:29:30 +0300

    24.4.23

--- code/test_matlab/test.m
+++ code/test_matlab/test.m
@@ -1,8 +1,8 @@
 realInput = (cell2mat(struct2cell(load('C:/Users/magshimim/Desktop/DPD/DPD/pa_data/input.mat'))));
 output = transpose(cell2mat(struct2cell(load('output.mat'))));
 input = transpose(create_matrix(realInput));
-load('(22 25 35 30 20 10 2DPD).mat')
 load('(22-40-2PA).mat');
+load('(22 25 35 30 20 10 2DPD).mat');
 
 without_dpd = netPA(input);
 without_dpd = without_dpd(1,:) + 1j*without_dpd(2,:);
@@ -33,6 +33,9 @@ title("Output spectrum with and without dpd");
 xlabel("f [Hz]");
 ylabel("Amplitude [dB]");
 
+oobwidpd = (norm((abs(withDPD(1100:4500))).^2)+norm((abs(withDPD(6000:9400))).^2))/norm((abs(withDPD(4500:6000))).^2);
+oobwodpd = (norm((abs(withoutDPD(1100:4500))).^2)+norm((abs(withoutDPD(6000:9400))).^2))/norm((abs(withoutDPD(4500:6000))).^2);
+OOB = 10*log10(oobwidpd/oobwodpd);
 
 figure(3);
 epsilon = 0.0000000000000000000000001;
@@ -80,4 +83,9 @@ title("NN's output and measured output spectrum");
 xlabel("f [Hz]");
 ylabel("Amplitude [dB]");
 
+mdl = fitlm(abs(realInput),abs(with_dpd));
+Gain = mdl.Coefficients.Estimate(2);
+desired = realInput*Gain;
+MMSE = 10*log10(norm(abs(desired) - abs(with_dpd))/norm(abs(desired)));
+R_squared = mdl.Rsquared.Ordinary;
 
